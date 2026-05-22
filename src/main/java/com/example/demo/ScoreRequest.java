4b8a3f106c8b30dfce23437e202af424daef9a9e
milkboy893 <m.kawaguchi@lincraft.co.jp>
2026-05-22 14:35:09 +0900

    コメント修正

--- src/main/java/com/example/demo/ScoreRequest.java
+++ src/main/java/com/example/demo/ScoreRequest.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 public class ScoreRequest implements Serializable {
     private String playerName;
-    private int score; // ★絶対に int（小文字）
+    private int score;
 
     public String getPlayerName() { return playerName; }
     public void setPlayerName(String playerName) { this.playerName = playerName; }
